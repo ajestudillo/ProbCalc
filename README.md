a6cb195f03541593c25b53268e1bcc5cf3d8243b
ajestudillo <ajestudillo@gmail.com>
2022-08-24 14:49:04 +0100

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+Psystat extracted from a fork of tomfaulkenberry/statShinyApps.
+
+For Alex's first-year course at BU.
---
+++ prob.R
@@ -0,0 +1,55 @@
+fluidRow(
+  
+  # input goes here
+  column(4,
+
+        ##change the title here
+        div(helpText('Probability calculator'), align = "left", style="font-size:140%"),
+        
+        br(),
+        
+        selectInput(inputId = "dist",
+                      label = "Distribution:",
+                      choices = c("Normal"      = "rnorm",
+                                  "t"           = "rt",
+                                  "F"           = "rf"),
+                      selected = "rnorm"),
+        uiOutput("mean"),
+        uiOutput("sd"),
+        uiOutput("N"),
+        # helpText("sd adjusted for number of participants"),
+        # div(textOutput("sdadj"),style="text-indent:20px;font-size:100%;"),
+        uiOutput("df_1"),
+        uiOutput("df_2"),
+        
+        br(),
+        br(),
+        
+        helpText("Region of p-value:"),
+        div(textOutput("region"),style="text-indent:20px;font-size:100%;"),
+        br(),
+        
+        uiOutput("tail"),
+        uiOutput("lower_bound"),
+        uiOutput("upper_bound"),
+        
+        
+        uiOutput("a"),
+        uiOutput("b")  
+  ), # close column 1
+
+  # output goes here
+  column(8,
+         tabsetPanel(
+           tabPanel("Summary",
+                  plotOutput("plot"),
+                  div(textOutput("area"), align = "center", style="font-size:150%;")
+                  ),
+           tabPanel("Help", 
+                    HTML("<iframe width=\"560\" height=\"315\" 
+                                 src=\"https://www.youtube.com/embed/ZL8Jtow7IlI\" 
+                                  frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>")
+           )
+         )
+  ) # close column
+) # close fluid row
